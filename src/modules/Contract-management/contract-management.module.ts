5bd2ed6a793e5f2613012ba3b6c34aa1ef89c38a
Karanveer <karanveer@devronins.com>
2025-05-21 11:55:26 +0530

    fix(contract-management) initialized ContractManagementDocumentRepository in module file; it does not give error.

--- src/modules/Contract-management/contract-management.module.ts
+++ src/modules/Contract-management/contract-management.module.ts
@@ -4,10 +4,11 @@ import { ContractManagementController } from './contract-management.controller';
 import { ContractManagementRepository } from './Repositories/contract-management.repository';
 import { OrganizationsModule } from 'modules/organizations/organizations.module';
 import { UsersModule } from 'modules/users/users.module';
+import { ContractManagementDocumentRepository } from './Repositories/contract-management-document.entity';
 
 @Module({
   imports:[OrganizationsModule,UsersModule],
   controllers: [ContractManagementController],
-  providers: [ContractManagementService,ContractManagementRepository],
+  providers: [ContractManagementService,ContractManagementRepository,ContractManagementDocumentRepository],
 })
 export class ContractManagementModule {}
